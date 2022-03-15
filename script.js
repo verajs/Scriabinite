dc7d826327dbade72344369f68669e33c09cee44
Sebastian Vera <100543895+verajs@users.noreply.github.com>
2022-03-15 17:44:00 -0400

    First commit

---
+++ script.js
@@ -0,0 +1,14 @@
+/* document.addEventListener('DOMContentLoaded', function() {
+
+let btn1 = document.querySelector('#btn1');
+let img1 = document.querySelector('#img1');
+btn1.addEventListener("mouseover", () => {
+img1.src = "images/steps/stair1hover.png"
+})
+
+btn1.addEventListener("mouseout", () => {
+img1.src = "images/steps/stair1.png"
+})
+
+
+}); */
