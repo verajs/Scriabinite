11866f58efceb79ed8b90d47b6c5cf55431058d2
Sebastian Vera <100543895+verajs@users.noreply.github.com>
2022-05-23 19:59:03 -0400

    v1.0.0

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-/* document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', function() {
 
 let btn1 = document.querySelector('#btn1');
 let img1 = document.querySelector('#img1');
@@ -11,4 +11,40 @@ img1.src = "images/steps/stair1.png"
 })
 
 
-}); */
+let btn2 = document.querySelector('#btn2');
+let img2 = document.querySelector('#img2');
+btn2.addEventListener("mouseover", () => {
+img2.src = "images/steps/stair2hover.png"
+})
+
+btn2.addEventListener("mouseout", () => {
+img2.src = "images/steps/stair2.png"
+})
+
+let btn3 = document.querySelector('#btn3');
+let img3 = document.querySelector('#img3');
+btn3.addEventListener("mouseover", () => {
+img3.src = "images/steps/stair3hover.png"
+})
+
+btn3.addEventListener("mouseout", () => {
+img3.src = "images/steps/stair3.png"
+})
+
+
+
+
+let btn4 = document.querySelector('#btn4');
+let img4 = document.querySelector('#img4');
+btn4.addEventListener("mouseover", () => {
+img4.src = "images/steps/stair4hover.png"
+})
+
+btn4.addEventListener("mouseout", () => {
+img4.src = "images/steps/stair4.png"
+})
+
+});
+
+
+
